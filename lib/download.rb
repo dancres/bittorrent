c8bee6550120c5969455a8ffbaca85f11b1e654a
Dan Creswell <dan.creswell@gmail.com>
2012-08-09 11:50:45 +0100

    Align with use of sha1_hash not hash for MetaInfo

--- lib/download.rb
+++ lib/download.rb
@@ -6,9 +6,11 @@ require './client.rb'
 #m = MetaInfo.new("../metas/cluj.torrent")
 # m = MetaInfo.new("../metas/aaron.torrent")
 #m = MetaInfo.new("../metas/eclipse-SDK-3.7-linux-gtk-x86_64.torrent")
-m = MetaInfo.new("../metas/jdk-7u4-macosx-x64.dmg.torrent")
+#m = MetaInfo.new("../metas/jdk-7u4-macosx-x64.dmg.torrent")
+#m = MetaInfo.new("../metas/Aquamacs Manual.pdf.torrent")
+m = MetaInfo.new("/Users/dan/Desktop/Sublime Text 2.0.1.dmg.torrent")
 
 c = ClientDetails.new(8080)
-t = Tracker.new(m.tracker, m.info.hash, c)
+t = Tracker.new(m.tracker, m.info.sha1_hash, c)
 d = Downloader.new(m, t, c)
 d.run
