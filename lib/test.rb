a2679e5cf145b4084eb6e4d7ab8375d7ff921a51
Dan Creswell <dan.creswell@gmail.com>
2012-08-09 11:51:08 +0100

    More meta files for test

--- lib/test.rb
+++ lib/test.rb
@@ -8,13 +8,15 @@ puts Tracker::INFO_HASH
 
 #m = MetaInfo.new("../metas/eclipse-SDK-3.7-linux-gtk-x86_64.torrent")
 
-m = MetaInfo.new("../metas/cluj.torrent")
+#m = MetaInfo.new("../metas/cluj.torrent")
 
 #m = MetaInfo.new("../metas/aaron.torrent")
+
+m = MetaInfo.new("/Users/dan/Desktop/Sublime Text 2.0.1.dmg.torrent")
 
 puts m.tracker
 puts m.info
 m.info.pieces.dump
 
-#t = Tracker.new(m.tracker, m.info.hash, nil, 8080)
+#t = Tracker.new(m.tracker, m.info.sha1_hash, nil, 8080)
 #puts t.ping({Tracker::DOWNLOADED => "12345"}).code
