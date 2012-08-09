88388340ca103e82eb1c3f2454d96d045c8e47b8
Dan Creswell <dan.creswell@gmail.com>
2012-08-09 11:51:47 +0100

    Add initial client connection steps

--- lib/downloader.rb
+++ lib/downloader.rb
@@ -1,6 +1,7 @@
 require 'socket'
 require './tracker.rb'
-require './connection.rb'
+require './io.rb'
+require './btproto.rb'
 
 class Downloader
 
@@ -8,6 +9,7 @@ class Downloader
 		@meta = metainfo
 		@tracker = tracker
 		@client_details = client_details
+		@core = Core.new(client_details)
 	end
 
 	def run
@@ -18,7 +20,7 @@ class Downloader
 		puts response
 
 		if (response.code == 200)
-			puts "Good"
+			puts "Good - going to pull: #{@meta.info.sha1_hash.unpack("H*")} #{@meta.info.pieces.pieces.length} pieces of length #{@meta.info.pieces.piece_length}"
 
 			tr = Tracker::AnnounceResponse.new(response.body)
 
@@ -28,17 +30,32 @@ class Downloader
 			my_addresses = Socket.ip_address_list.map { |addr| addr.ip_address}
 
 			tr.peers.each { |peer|
-				if (my_addresses.include?(peer.ip.ip_address) && (peer.port == @client_details.port))
-					puts "Discarding: #{peer}"
-				else
+				if (! ((my_addresses.include?(peer.ip.ip_address) && (peer.port == @client_details.port))))
 					puts "Connecting: #{peer}"
 
+					socket = TCPSocket.new(peer.ip.ip_address, peer.port)
+					Connection.new(socket, Connection::SEND_HANDSHAKE, @meta, @core)
 				end
 			}
 
+			sleep(60)
 		else
 			puts "Bad #{response.code}"
 			return
 		end
 	end	
 end
+
+class Core
+	attr_reader :selector, :serversocket, :client_details
+
+	def initialize(client_details)
+		@selector = Selector.new
+		@client_details = client_details
+		@serversocket = TCPServer.new(client_details.port)
+	end
+
+	def terminate
+	end
+end
+
