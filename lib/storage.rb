4cc107a55e295d96693f85fe94edeae735c80ce0
Dan Creswell <dan.creswell@gmail.com>
2012-08-17 01:19:43 +0100

    Move blocks handling into Storage

--- lib/storage.rb
+++ lib/storage.rb
@@ -1,14 +1,30 @@
 require_relative 'bitset.rb'
 
 class Storage
+	BLOCK_SIZE = 16384
+
 	attr_reader :got
 
-	def initialize(size)
+	def initialize(size, piece_length)
 		@size = size
 		@got = Bitset.new(size).fill(0)
+		@piece_length = piece_length
 	end
 
 	def needed
 		@got.invert
 	end
+
+	def blocks
+		total = @piece_length / BLOCK_SIZE
+
+		requests = (0...total).map { |b| [b * BLOCK_SIZE, BLOCK_SIZE]}
+
+		if ((@piece_length % BLOCK_SIZE) != 0)
+			requests << [total * BLOCK_SIZE, @piece_length % BLOCK_SIZE]
+		end
+
+		requests
+	end
+
 end
