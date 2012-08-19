02e514b1dcb6a070c45890c43617c4c2aebb0471
Dan Creswell <dan.creswell@gmail.com>
2012-08-19 16:53:56 +0100

    Initial disk-writing scheme
    Doesn't yet handle overlap of block across files in the torrent.

--- lib/storage.rb
+++ lib/storage.rb
@@ -3,15 +3,39 @@ require_relative 'bitset.rb'
 class Storage
 	BLOCK_SIZE = 16384
 
-	attr_reader :got, :overall_bytes, :current_bytes
+	attr_reader :got, :overall_bytes, :current_bytes, :handles
 
-	def initialize(metainfo)
+	def initialize(directory, metainfo)
 		@metainfo = metainfo
 		@size = metainfo.info.pieces.pieces.length
 		@got = Bitset.new(@size).fill(0)
 		@piece_length = metainfo.info.pieces.piece_length
 		@overall_bytes = @metainfo.info.directory.files.inject(0) { |base, f| base + f.length}
 		@current_bytes = 0
+
+		offset = 0
+		@handles = {}
+
+		@metainfo.info.directory.files.each { | f |
+			current = offset
+			offset += f.length
+			@handles[ current..current + f.length - 1 ] = File.new("#{directory}#{File::SEPARATOR}#{f.name}", "a+b")
+		}
+	end
+
+	def locate(offset)
+		handles.keys.inject(nil) { | chosen, r |
+			if (r.cover?(offset))
+				r
+			else
+				chosen
+			end
+		}
+	end
+
+	def close
+		@handles.values.each { | f | f.close }
+		@handles = {}
 	end
 
 	def needed
@@ -22,7 +46,19 @@ class Storage
 		return (current_bytes == overall_bytes)
 	end
 
-	def save_block(piece, block, data)
+	def save_block(piece, block_range, data)
+		abs_blk_pos = (piece * @piece_length) + block_range[0]
+		range = locate(abs_blk_pos)
+		file_blk_pos = abs_blk_pos - range.begin
+		bytes_to_write = [range.end + 1 - file_blk_pos, data.length].min
+
+		handle = @handles[range]
+
+		puts "Seeking to: #{file_blk_pos} to write #{bytes_to_write} => #{abs_blk_pos} #{range}"
+
+		handle.seek(file_blk_pos, IO::SEEK_SET)
+		handle.write(data)
+
 		@current_bytes += data.length
 	end
 
