dee001171e9522cc01040abb1e944f2dfc26b687
Dan Creswell <dan.creswell@gmail.com>
2012-08-23 21:08:32 +0100

    Make loggers provide line trace and add logger for picker.

--- configure/environment.rb
+++ configure/environment.rb
@@ -4,20 +4,69 @@ $stdout.sync = true
 
 DEFAULT_OUTPUT = STDOUT
 
+original_formatter = Logger::Formatter.new
+
+def dumpLogger(stack)
+	offset = 0
+	while (stack[offset].include?("logger.rb"))
+		offset += 1
+	end
+
+	stack.drop(offset)[0]
+end
+
 COLLECTOR_LOGGER = Logger.new(DEFAULT_OUTPUT)
 COLLECTOR_LOGGER.level = Logger::DEBUG
+COLLECTOR_LOGGER.formatter = proc { |severity, datetime, progname, msg|
+	trace = Kernel::caller
+	trace = (trace != nil) ? dumpLogger(trace) : "--xxx--"
+	original_formatter.call(severity, datetime, progname, "#{trace} #{msg.dump}")
+}
 
 CHOKER_LOGGER = Logger.new(DEFAULT_OUTPUT)
-CHOKER_LOGGER.level = Logger::INFO
+CHOKER_LOGGER.level = Logger::DEBUG
+CHOKER_LOGGER.formatter = proc { |severity, datetime, progname, msg|
+	trace = Kernel::caller
+	trace = (trace != nil) ? dumpLogger(trace) : "--xxx--"
+	original_formatter.call(severity, datetime, progname, "#{trace} #{msg.dump}")
+}
 
 CONNECTION_LOGGER = Logger.new(DEFAULT_OUTPUT)
-CONNECTION_LOGGER.level = Logger::INFO
+CONNECTION_LOGGER.level = Logger::DEBUG
+CONNECTION_LOGGER.formatter = proc { |severity, datetime, progname, msg|
+	trace = Kernel::caller
+	trace = (trace != nil) ? dumpLogger(trace) : "--xxx--"
+	original_formatter.call(severity, datetime, progname, "#{trace} #{msg.dump}")
+}
 
 STORAGE_LOGGER = Logger.new(DEFAULT_OUTPUT)
 STORAGE_LOGGER.level = Logger::INFO
+STORAGE_LOGGER.formatter = proc { |severity, datetime, progname, msg|
+	trace = Kernel::caller
+	trace = (trace != nil) ? dumpLogger(trace) : "--xxx--"
+	original_formatter.call(severity, datetime, progname, "#{trace} #{msg.dump}")
+}
 
 SELECTOR_LOGGER = Logger.new(DEFAULT_OUTPUT)
 SELECTOR_LOGGER.level = Logger::INFO
+SELECTOR_LOGGER.formatter = proc { |severity, datetime, progname, msg|
+	trace = Kernel::caller
+	trace = (trace != nil) ? dumpLogger(trace) : "--xxx--"
+	original_formatter.call(severity, datetime, progname, "#{trace} #{msg.dump}")
+}
 
 TRACKER_LOGGER = Logger.new(DEFAULT_OUTPUT)
 TRACKER_LOGGER.level = Logger::INFO
+TRACKER_LOGGER.formatter = proc { |severity, datetime, progname, msg|
+	trace = Kernel::caller
+	trace = (trace != nil) ? dumpLogger(trace) : "--xxx--"
+	original_formatter.call(severity, datetime, progname, "#{trace} #{msg.dump}")
+}
+
+PICKER_LOGGER = Logger.new(DEFAULT_OUTPUT)
+PICKER_LOGGER.level = Logger::INFO
+PICKER_LOGGER.formatter = proc { |severity, datetime, progname, msg|
+	trace = Kernel::caller
+	trace = (trace != nil) ? dumpLogger(trace) : "--xxx--"
+	original_formatter.call(severity, datetime, progname, "#{trace} #{msg.dump}")
+}
